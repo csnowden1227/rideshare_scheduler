0d7c572c8d8651b5a532ab4f5f8b8847b82bd6aa
csnowden1227 <csnowden@csgroupmarketing.com>
2026-04-01 14:52:48 -0700

    Raise save payload limit and move toasts

--- server.js
+++ server.js
@@ -207,7 +207,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // --- IFRAME & SECURITY POLICY ---
